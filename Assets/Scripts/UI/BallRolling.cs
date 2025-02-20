f413c1f73e1d0e3209be878379b9e583a821c41a
Måns Fritiofsson <mans@fritiofson.net>
2025-02-20 12:22:44 +0100

    Correct ball rotation finally

--- Assets/Scripts/UI/BallRolling.cs
+++ Assets/Scripts/UI/BallRolling.cs
@@ -8,9 +8,10 @@ public class BallRolling : MonoBehaviour
 
     void Update()
     {
-        Vector3 axis = Vector3.Cross(rigidbodyToFollow.velocity, Vector3.forward);
+        Vector3 axis = Vector3.Cross(rigidbodyToFollow.velocity.normalized, Vector3.forward);
         Debug.DrawRay(transform.position, axis);
-        float angle = -rigidbodyToFollow.velocity.magnitude * Mathf.Rad2Deg * Time.deltaTime;
-        transform.Rotate(axis, angle);
+        Debug.DrawRay(transform.position, transform.forward, Color.blue);
+        float angle = rigidbodyToFollow.velocity.magnitude * Mathf.Rad2Deg * Time.deltaTime;
+        transform.Rotate(axis, angle, Space.World);
     }
 }
